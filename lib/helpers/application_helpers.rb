1aed993b927170fe322a21734105ac6d3687aaf4
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-09-29 22:25:30 +0200

    Use url as company link text

--- lib/helpers/application_helpers.rb
+++ lib/helpers/application_helpers.rb
@@ -2,4 +2,9 @@ module ApplicationHelpers
   def render_markdown(markdown)
     Kramdown::Document.new(markdown).to_html
   end
+
+  def external_link(url)
+    link_text = URI.parse(url).host
+    link_to(link_text, url, target: '_blank')
+  end
 end
